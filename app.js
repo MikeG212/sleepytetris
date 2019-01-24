9c0eaface0e12589f4b7e2344b7e4e9a38d2be05
MikeG212 <michaelcgee93@gmail.com>
2019-01-24 15:32:31 -0800

    Add instructions and remove old code

--- app.js
+++ app.js
@@ -1,394 +1,388 @@
-let world = document.getElementById('world');
-
-const configs = ["pos1", "pos2", "pos3", "pos4"];
-
-let next = document.getElementById("next");
-let colorMap = {
-    0: "black",
-    1: "red",
-    2: "orange",
-    3: "yellow",
-    4: "green",
-    5: "blue",
-    6: "indigo",
-    7: "pink",
-
-}
-
-
-
-let scoreBoard = document.getElementById('scoreBoard');
-let gameRunning = true;
-
-const SHAPES = {
-    o: [[1, 1], 
-        [1, 1]],
-
-    t: [[2, 2, 2]
-        [0, 2, 0],
-        [0, 0, 0]],
-
-    i: [[3, 3, 3, 3],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]],
-
-    l: [[4, 4, 4]
-        [4, 0, 0],
-        [0, 0, 0]],
-
-    l: [[5, 5, 5]
-        [0, 0, 5],
-        [0, 0, 0]],
-
-    s: [[0, 6, 6]
-        [6, 6, 0],
-        [0, 0, 0]],
-
-    z: [[7, 7, 0]
-        [0, 7, 7],
-        [0, 0, 0]],
-};
-
-function deepDup(arr) {
-    return arr.map(el => {
-        if (el instanceof Array) {
-            return deepDup(el)
-        } else {
-            return el;
-        }
-    });
-
-}
-
-function incrementCoord(y, x) {
-  currentShape.indices.forEach(coord => {
-    coord[0] += y;
-    coord[1] += x;
-  });
-  currentShape.origin[0] += y;
-  currentShape.origin[1] += x
-}
-
-
-
-let shapeKeyBag = replenishShapeBag();
-let currentShape;
-let height = 15;
-let width = 10;
-let center = Math.floor(width / 2);
-let state = 1;
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+const scoreBoard = document.getElementById("scoreBoard");
+const next = document.getElementById("next")
+let running = true;
+const grid = createMatrix(12, 20);
+let midpoint = (grid[0].length / 2) | 0;
+let isI;
 let score = 0;
+const colorMap = [
+  "black",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "teal",
+  "indigo",
+  "pink",
+  "white"
+];
+let shapeBag = replenishShapeBag().concat(replenishShapeBag());
+let dropCounter = 0;
+let dropInterval = 250;
+let lastTime = 0;
+let keepHardDropping = true;
 let gameOver = false;
 
-function resetGrid() {
-    return [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ];
-} 
+context.scale(20, 20);
 
-let grid = resetGrid();
-
-const vectorMap = {
-    "left": { y: 0, x: -1, edge: 0},
-    "right": { y: 0, x: 1, edge: grid[0].length - 1},
-    "down": { y: 1, x: 0, edge: grid.length - 1}
+const currentPiece = {
+    pos: { x: midpoint, y: -2 },
+    matrix: createPiece(randomType())
 };
-
-
-
-document.onkeydown = function (e) {
-    let direction;
-    e.preventDefault();
-    switch (e.keyCode) {
-        case 40:
-            direction = "down";
-            score++;
-            break;
-        case 37:
-            direction = "left";
-            break;
-        case 38:
-            direction = "up";
-            break;
-        case 39:
-            direction = "right";
-            break;
-        case 32:
-            direction = "space";
-            break;
-        default:
-            break;
-    }
-    moveShapes(direction)
-    scoreBoard.innerHTML = `Score: ${score}`;
-    drawWorld();
-}
-
-let resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", () => {
-    grid = resetGrid();
-    score = 0;
-    start();
-});
 
 let pauseButton = document.getElementById("pause-button");
 pauseButton.addEventListener("click", () => {
-  gameRunning = false;
+    running = !running;
+    if (!running) {
+        pauseButton.innerHTML = "Resume";
+    } else {
+        pauseButton.innerHTML = "Pause";
+    }
 });
 
-function replenishShapeBag() {
-    return shuffle(Object.keys(SHAPES));
+let resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", () => {
+    resetGame()
+});
+
+function resetGame() {
+    grid.forEach(row => row.fill(0));
+    resetPiece();
+    score = 0;
+    dropInterval = 250;
+    gameOver = false;
+    canvas.style.opacity = 1;
+    running = true;
+    resetButton.innerHTML = "Reset";
+
+}
+
+document.onkeydown = function (e) {
+    if (gameOver) {
+        resetGame();
+    }
+    e.preventDefault();
+    if (!running) {
+        return;
+    }
+    switch (e.keyCode) {
+        case 40: //down
+            drop();
+            score++;
+            break;
+        case 37: //left
+            move(-1);
+            break;
+        case 39: //right
+            move(1);
+            break;
+        case 32: //space
+            hardDrop();
+            break;
+        case 38: //up
+            currentPieceRotate(1);
+            break;
+        case 87: //w
+            currentPieceRotate(-1);
+            break;
+        default:
+            break;
+    }
+};
+
+function setNext() {
+    next.innerHTML = `Next: ${shapeBag.slice(0, 4)}`;
 }
 
 function shuffle(array) {
-    let m = array.length, t, i;
-    while (m) {
-        i = Math.floor(Math.random() * m--);
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
-    }
-    return array;
+  let m = array.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
 }
 
-function createShape() {
+function replenishShapeBag() {
+    return shuffle("ITOLSZJ".split(""));
+    
+}
+
+function randomType() {
+    const type = shapeBag.shift();
+    if (shapeBag.length < 7) {
+        shapeBag = shapeBag.concat(replenishShapeBag()).concat(replenishShapeBag());
+    }
     setNext();
-    if (shapeKeyBag.length < 4) {
-        let nextShapes = replenishShapeBag();
-        shapeKeyBag = shapeKeyBag.concat(nextShapes);        
-    }
-    currentShape = SHAPES[shapeKeyBag.shift()];
-    let start = [0, center];
-
-    currentShape.indices.forEach(coord => {
-        newY = location[0] + coord[0];
-        newX = center + coord[1];
-        grid[newY][newX] = currentShape.val;
-        coord[0] = newY;
-        coord[1] = newX;
-    })
-    debugger
-
-    currentShape.origin[1] += center;
+    return type;
 }
 
-function drawWorld() {
-    scoreBoard.innerHTML = `Score: ${score}`;
-    while (world.lastChild) {
-        world.removeChild(world.lastChild);
-    }
-    let square;
-    for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid[y].length; x++) {
-            // if (y < 2) {
-            //     continue;
-            // }
-            square = document.createElement('div');
-            square.classList.add("empty");
-            square.style.backgroundColor = colorMap[grid[y][x] % 10];
-            if (grid[y][x] === 0) {
-                square.classList.add("empty");
-            } else {
-                square.classList.add("squareShape");
-            }
-            document.getElementById("world").appendChild(square);
-        }
-        world.innerHTML += "<br>"
-    }
-}
+function createPiece(type) {
+    type === "I" ? isI = true : isI = false;
+    switch (type) {
+        case "T":
+            return [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0]
+            ];
+            // break;
+        case "O":
+            return [
+                [2, 2],
+                [2, 2]
+            ];
+            // break;
+        case "I":
+            return [
+                [0, 0, 0, 0],
+                [3, 3, 3, 3],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
 
-function containsLiveBlock(y, x) {
-    return grid[y][x] > 0 && grid[y][x] < 10;
-}
-
-
-function moveOver(vector, y, x) {
-    if (containsLiveBlock(y, x)) {
-        grid[y + vector.y][x + vector.x] = grid[y][x];
-        grid[y][x] = 0;
-    }
-}
-
-function setNext() {
-    next.innerHTML = `Next: ${shapeKeyBag.slice(0,4)}`;
-}
-
-
-function moveShapes(direction) {
-    let canMove = true
-    let vector = vectorMap[direction];
-    switch (direction) {
-        case "up":
-            rotateCW();
-            break;
-        case "space":
-            hardDown();
-            break;
-        case "left":
-            for (let y = grid.length - 1; y >= 0; y--) {
-              for (let x = 0; x < grid[y].length; x++) {
-                if (containsLiveBlock(y, x)) {
-                  if (x === vector.edge || grid[y + vector.y][x + vector.x] > 10) {
-                    canMove = false;
-                  }
-                }
-              }
-            }
-
-            if (canMove) {
-                for (let y = grid.length - 1; y >= 0; y--) {
-                    for (let x = 0; x < grid[y].length; x++) {
-                        moveOver(vector, y, x);
-                        }
-                }
-                incrementCoord(vector.y, vector.x);
-            }
-            break;
-        
-        case "right":
-            for (let y = grid.length - 1; y >= 0; y--) {
-              for (let x = grid[y].length; x >= 0; x--) {
-                if (containsLiveBlock(y, x)) {
-                  if (x === vector.edge || grid[y + vector.y][x + vector.x] > 10) {
-                    canMove = false;
-                  }
-                }
-              }
-            }
-
-            if (canMove) {
-                for (let y = grid.length - 1; y >= 0; y--) {
-                    for (let x = grid[y].length; x >= 0; x--) {
-                            moveOver(vector, y, x);
-                        }
-                }
-                incrementCoord(vector.y, vector.x);
-            }
-            break;
-
-        case "down":
-            for (let y = grid.length - 1; y >= 0; y--) {
-                for (let x = 0; x < grid[y].length; x++) {
-                    if (containsLiveBlock(y, x)) {
-                        if (y === vector.edge || grid[y + vector.y][x + vector.x] > 10) {
-                          canMove = false;
-                          freeze();
-                        }
-                    }
-                }
-            }
-            if (canMove) {
-                for (let y = grid.length - 1; y >= 0; y--) {
-                    for (let x = 0; x < grid[y].length; x++) {
-                        moveOver(vector, y, x);
-                    }
-                }
-                incrementCoord(vector.y, vector.x);
-            }
-            break;
-
+            ];
+            // break;
+        case "L":
+            return [
+                [4, 4, 4],
+                [4, 0, 0],
+                [0, 0, 0]
+            ];
+            // break;
+        case "J":
+            return [
+                [5, 5, 5],
+                [0, 0, 5],
+                [0, 0, 0]
+            ];
+            // break;
+        case "S":
+            return [
+                [0, 6, 6],
+                [6, 6, 0],
+                [0, 0, 0]
+            ];
+            // break;
+        case "Z":
+            return [
+                [7, 7, 0],
+                [0, 7, 7],
+                [0, 0, 0]
+            ];
+            // break;    
         default:
             break;
     }
 }
 
-function gameOverCheck() {
-    if (grid[0].filter(cell => !!cell).length || grid[1].filter(cell => !!cell).length) {
-        gameOver = true;
-        alert("GAME OVER");
-    }
+function draw() {
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawGrid(grid, {x: 0, y: 0});
+    drawMatrix(currentPiece.matrix, currentPiece.pos);
 }
 
-function freeze() {
-    for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid[y].length; x++) {
-            if (containsLiveBlock(y, x)) {
-                grid[y][x] += 10;
+function collide(grid, currentPiece) {
+    const [matrix, pos] = [currentPiece.matrix, currentPiece.pos]
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] &&
+                (grid[y + pos.y] &&
+                grid[y + pos.y][x + pos.x]) !==0) {
+                return true;
             }
         }
     }
-    clearLines();
-    gameOverCheck();
-    createShape();
+    return false;
 }
 
-function hardDown() {
-
-}
-
-function rotateCW() {
-    debugger
-    let origin = currentShape.origin;
-    let y;
-    let x;
-    for (let col = 0; col < 3; col ++) {
-        for (let row = 0; row < 3; row++) {
-            y = col - origin[0];
-            x = row - origin[1];
-
-            grid[col + displace.y][row + displace.x] = currentShape.val;
-        }
+function createMatrix(width, height) {
+    const matrix = [];
+    while (matrix.length < height) {
+        matrix.push(new Array(width).fill(0));
     }
-    currentShape.indices.forEach(coord => {
-        coord[0] += displace.y;
-        coord[1] += displace.x;
-        grid [coord[0], coord[1]] = currentShape.val;
+    return matrix;
+}
+
+function merge(grid, currentPiece) {
+    currentPiece.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value) {
+                grid[y + currentPiece.pos.y][x + currentPiece.pos.x] = value;
+            }
+        });
     });
 }
 
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value) {
+                context.fillStyle = colorMap[value];
+                context.fillRect(x + offset.x, y + offset.y, 1, 1)
+            }
+        });
+    });
+}
+
+function drawGrid(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value >= 0) {
+        context.fillStyle = colorMap[value];
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+    updateScore();
+    if (dropCounter > dropInterval && running) {
+        drop();
+    }
+    draw();
+    requestAnimationFrame(update);
+}
+
 function clearLines() {
-    for (let y = 0; y < grid.length; y++) {
-        counter = 0;
+    counter = 0;
+    for (let y = 0; y < grid.length; y++) {   
         if (!grid[y].includes(0)) {
             counter++;
-            grid.splice(y, 1);
-            grid.unshift([0,0,0,0,0,0,0,0,0,0]);
-            
+            const row = grid.splice(y, 1)[0].fill(0);
+            grid.unshift(row);
+            if (dropInterval > 100) {
+                dropInterval -= 50;
+            } else if (dropInterval > 50) {
+                dropInterval -= 10;
+            }
         }
-        switch (counter) {
-          case 1:
+    }
+    
+    switch (counter) {
+        case 1:
             score += 100;
             break;
-          case 2:
+        case 2:
             score += 250;
             break;
-          case 3:
+        case 3:
             score += 400;
             break;
-          case 4:
+        case 4:
             score += 500;
             break;
-          default:
+        default:
             break;
-        }
     }
 }
 
-function gameLoop() {
-    drawWorld();
-    moveShapes("down");
-    setTimeout(gameLoop, 500);
+function drop() {
+    keepHardDropping = true;
+    currentPiece.pos.y++;
+    if (collide(grid, currentPiece)) {
+        currentPiece.pos.y--;
+        keepHardDropping = false;
+        if (dropInterval > 100) {
+            dropInterval -= 1;
+        }
+        merge(grid, currentPiece);
+        clearLines();
+        resetPiece();
+    }
+    dropCounter = 0;
+}
+function resetPiece() {
+    currentPiece.type = randomType();
+    currentPiece.matrix = createPiece(currentPiece.type);
+    currentPiece.pos.y = 0;
+    currentPiece.pos.x = midpoint - (currentPiece.matrix[0].length / 2 | 0);
+    if (collide(grid, currentPiece)) {
+        currentPiece.pos.y -= 1;
+        endGame();
+    }
 }
 
-function start() {
-    createShape();
-    gameLoop();
+function endGame() {
+    canvas.style.opacity = .5;
+    gameOver = true;
+    running = false;
+    resetButton.innerHTML = "New Game";
+}
+
+function move(dir) {
+    currentPiece.pos.x += dir;
+    if (collide(grid, currentPiece)) {
+       currentPiece.pos.x -= dir;
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < y; x++ ) {
+            [
+                matrix[x][y],
+                matrix [y][x]
+            ] = [
+                matrix[y][x],
+                matrix[x][y]
+            ];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse())
+    } else {
+        matrix.reverse();
+    }
+}
+
+function currentPieceRotate(dir) {
+  const pos = currentPiece.pos.x;
+  let offset = 1;
+  rotate(currentPiece.matrix, dir);
+  while (collide(grid, currentPiece)) {
+
+    if (isI) {
+      currentPiece.matrix.forEach(row => row.reverse());
+      if (currentPiece.pos.x <= 0) {
+        currentPiece.pos.x = -1;
+      } else {
+        currentPiece.pos.x = 7;
+      }
+    }
+    currentPiece.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > currentPiece.matrix[0].length) {
+      rotate(currentPiece.matrix, -dir);
+      currentPiece.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function updateScore() {
+    scoreBoard.innerHTML = `Score: ${score}`;
+}
+
+function hardDrop() {
+    while (keepHardDropping) {
+        drop();
+        score++;
+    }
 
 }
 
-start();
+resetGame();
+update();
+
